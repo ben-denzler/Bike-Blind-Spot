858a8afbca11529e202ecc12b1ede0b09f44b2a5
Benjamin Denzler <bdenz001@ucr.edu>
2022-04-25 12:39:44 -0700

    Initial sensor implementation
    Got a single ultrasonic sensor working with the task scheduler

--- main/main.ino
+++ main/main.ino
@@ -1,6 +1,13 @@
 #include <TimerOne.h>
+#include <SR04.h>
 
 // Serial.print("GCD is: "); Serial.print(GCD); Serial.print('\n');   // DEBUGGING
+
+#define LEFT_ECHO_PIN 11
+#define LEFT_TRIG_PIN 12
+
+SR04 LeftSensor = SR04(LEFT_ECHO_PIN, LEFT_TRIG_PIN);
+long LeftSensorDistance;
 
 // Computes GCD of two numbers
 unsigned long gcd(unsigned long a, unsigned long b) {
@@ -21,8 +28,8 @@ typedef struct task {
   int (*TickFct)(int);          // Address of tick function
 } task;
 
-static task task1;
-task* tasks[] = { &task1 };
+static task task1, task2;
+task* tasks[] = { &task1, &task2 };
 const unsigned char numTasks = sizeof(tasks) / sizeof(tasks[0]);
 const char startState = 0;    // Refers to first state enum
 unsigned long GCD = 0;        // For timer period
@@ -30,6 +37,15 @@ unsigned long GCD = 0;        // For timer period
 // Task 1 (Blinks the onboard LED)
 enum BL_States { BL_SMStart, BL_On, BL_Off };
 int TickFct_BlinkLED(int state);
+
+// Task 2 (Outputs distance from left ultrasonic sensor)
+enum LS_States { LS_SMStart };
+int TickFct_LeftSensor(int state) {
+  LeftSensorDistance = LeftSensor.Distance();
+  Serial.print(LeftSensorDistance);
+  Serial.print("cm\n");
+  return LS_SMStart;
+}
 
 void setup() {
   unsigned char j = 0;
@@ -39,6 +55,13 @@ void setup() {
   tasks[j]->period = 1000000;
   tasks[j]->elapsedTime = tasks[j]->period;
   tasks[j]->TickFct = &TickFct_BlinkLED;
+  ++j;
+
+  // Task 2 (Outputs distance from left ultrasonic sensor)
+  tasks[j]->state = startState;
+  tasks[j]->period = 1000000;
+  tasks[j]->elapsedTime = tasks[j]->period;
+  tasks[j]->TickFct = &TickFct_LeftSensor;
   ++j;
 
   // Find GCD for timer's period
